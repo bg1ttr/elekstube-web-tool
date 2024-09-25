b55008ad2fc650361330f7762f825ff2d50e8e5c
ZIYI WANG <wangziyi@me.com>
2024-09-25 18:12:20 +0900

    更新代码

--- profile.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav ul li a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            navLinks.forEach(nav => nav.classList.remove('active'));
-            event.target.classList.add('active');
-        });
-    });
-});
